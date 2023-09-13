f6b1767616cdf2917d520e3518410565549aa7a4
n0xgg04 <dulieu.vblc@gmail.com>
2023-09-13 16:04:01 +0700

    update: done task

--- App.tsx
+++ App.tsx
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, Dimensions } from "react-native";
+import React from "react";
+import { useDeviceContext } from "twrnc";
+import tw from "./utils/libs/twrnc";
+import { ScrollView } from "react-native";
+import { useFonts } from 'expo-font';
+import TopBar from "@components/TopBar";
+import PlaySuggestion from "@components/PlaySuggestion";
+import PickedForYou from "@components/PickedForYou";
+import BottomNavigation from "@components/BottomNavigation";
 
 export default function App() {
+  //async load font
+  useDeviceContext(tw);
+  const [loaded] = useFonts({
+    CircularBold: require("./assets/fonts/Circular/CircularStd-Bold.otf"),
+    CircularRegular: require("./assets/fonts/Circular/CircularStd-Book.otf"),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
+  const { width, height } = Dimensions.get("window");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <React.Fragment>
+      <StatusBar style="light" translucent />
+      <ScrollView style={tw`relative bg-primary h-[${height}px] w-full px-1`}>
+        <TopBar />
+        <PlaySuggestion />
+        <PickedForYou />
+      </ScrollView>
+      <BottomNavigation />
+    </React.Fragment>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,26 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      ["@babel/plugin-transform-flow-strip-types"],
+      ["@babel/plugin-proposal-decorators", { "legacy": true }],
+      ["@babel/plugin-proposal-class-properties", { "loose": true }],
+      ["module-resolver", {
+        "alias": {
+          "@navigation": "./navigation",
+          "@components": "./components",
+          "@screens": "./screens",
+          "@stores": "./stores",
+          "@assets": "./assets"
+        },
+        "extensions": [
+          ".js",
+          ".jsx",
+          ".ts",
+          ".tsx",
+        ]
+      }],
+    ]
   };
 };
